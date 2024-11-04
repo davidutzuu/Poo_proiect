027c0f8245a89a1ee601cd4a3fdc2407519693de
davidutzuu <148719042+davidutzuu@users.noreply.github.com>
2024-11-04 19:22:49 +0200

    ball.0

--- 11/ball.h
+++ 11/ball.h
@@ -1,14 +1,1 @@
-#ifndef BRICK_H
-#define BRICK_H // din nou, include guard pentru a preveni includerea multipla
-#include <iostream>
-
-class Brick { // Definirea clasei Brick
-    int x, y; // variabile pentru poziția cărămizii
-    bool destroyed; // Dvariabila pentru starea cărămizii (distrusă sau nu)
-public:
-    Brick(int x, int y, bool destroyed); // constructor cu parametri
-    friend std::ostream& operator<<(std::ostream &os, const Brick &brick); // Declarația operatorului de afișare
-};
-
-#endif // BRICK_H
 
